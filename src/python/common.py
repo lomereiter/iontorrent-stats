4906272153a4167bb615c3d36248b1902d0328a7
lomereiter <lomereiter@gmail.com>
2012-12-20 20:02:27 +0400

    unified indel/overall intensity plots

--- src/python/common.py
+++ src/python/common.py
@@ -7,3 +7,18 @@ from matplotlib.mlab import csv2rec, rec_groupby
 
 def read_table(filename, converter_dict=None, **kwargs):
     return csv2rec(filename, delimiter='\t', converterd=converter_dict, **kwargs)
+
+def plot_intensity_distribution(data, out_fn, xlabel="Intensity"):
+    approx_density = (data['count'] / 0.01) / np.sum(data['count'])
+
+    intensities = data['intensity'] / 100.0
+
+    plt.plot(intensities, approx_density, 'b-')
+    plt.fill_between(intensities, approx_density, color="grey", alpha=0.8)
+    plt.xticks(np.arange(0.5, np.max(intensities)))
+    plt.xlabel(xlabel)
+    plt.ylabel("Probability density")
+    plt.xlim(xmin=0)
+    plt.ylim(ymin=0)
+
+    plt.savefig(out_fn)
--- src/python/signal_intensities.py
+++ src/python/signal_intensities.py
@@ -5,15 +5,4 @@ def plot(in_fn, out_fn):
                        ('intensity', ),
                        [('count', sum, 'count')])
 
-    intensities = data['intensity'] / 100.0
-
-    plt.xticks(np.arange(0.5, np.max(intensities)))
-    plt.hist(intensities,
-             bins=200, 
-             weights=data['count'],
-             normed=True)
-
-    plt.xlabel('Signal intensity')
-    plt.ylabel('Probability density')
-
-    plt.savefig(out_fn)
+    plot_intensity_distribution(data, out_fn, "Signal intensity")
